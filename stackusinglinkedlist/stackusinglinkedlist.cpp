75d3f97d6a17ee2d55514ce8ba35f1b2e3d2d3ed
galang48 <galangyudha1945@gmail.com>
2024-05-11 11:27:57 +0700

    menyempurnakan int push

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -33,6 +33,9 @@ public:
 		newNode->data = value; // 2. Assign value
 		newNode->next = top; // 3. Set the next pointer of the new node to the current top node
 		top = newNode; // 4. update the top pointer to the new node
+		cout << "Push value: " << value << endl;
+		return value;
+	}
 
 
 
@@ -40,5 +43,3 @@ int main()
 {
     
 }
-
-
